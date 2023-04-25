19215269a26c59327e18188993bf4e7682827db6
Krish Katyl <katyalk@student.douglascollege.ca>
2023-04-24 18:02:23 -0700

    finall

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ const cors = require('cors');
 const app = express();
 const port = 5000;
 
-const dbURI = 'mongodb+srv://krishkatyal05:krish@cluster0.4xahymr.mongodb.net/BookList?retryWrites=true&w=majority';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+const URI = 'mongodb+srv://krishkatyal05:krish@cluster0.4xahymr.mongodb.net/BookList?retryWrites=true&w=majority';
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to database'))
   .catch(err => console.log(err));
 
